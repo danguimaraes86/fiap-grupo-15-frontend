74048afa7d985027cb704bce4d9af8b231832635
Daniel Guimarães <danguimaraes86@gmail.com>
2025-07-16 15:46:25 -0300

    refact: dashboard: melhorar props e utilizar config endpoint

--- mfe-dashboard/src/components/TransactionForm/index.tsx
+++ mfe-dashboard/src/components/TransactionForm/index.tsx
@@ -1,26 +1,26 @@
+import { useState } from "react";
 import img_transactions_left from "../../assets/images/transactions-left.png";
 import img_transactions_right from "../../assets/images/transactions-right.png";
+import {
+  Categoria,
+  getCategorias,
+  getTiposTransacao,
+  type TipoTransacao,
+} from "../../services/configs";
 import styles from "./TransactionForm.module.css";
 
 type Props = {
-  title: string;
-  options: string[];
-  selectedOption: string;
-  value: string;
-  onChangeValue: (val: string) => void;
-  onChangeOption: (val: string) => void;
-  onSubmit: () => void;
+  onSubmit: (selectedOption: string, transactionValue: string) => void;
 };
 
-export function TransactionForm({
-  title,
-  options,
-  selectedOption,
-  value,
-  onChangeValue,
-  onChangeOption,
-  onSubmit,
-}: Props) {
+const transactionOptions: TipoTransacao = await getTiposTransacao();
+const categoriaOptions: Categoria = await getCategorias();
+console.log(categoriaOptions);
+
+export function TransactionForm({ onSubmit }: Props) {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [transactionValue, setTransactionValue] = useState("");
+
   return (
     <div className={`${styles.box}`}>
       <img
@@ -34,24 +34,18 @@ export function TransactionForm({
         className={`${styles["transaction-decoration"]} ${styles.right}`}
       />
 
-      <h6 className={styles.title}>{title}</h6>
+      <h6 className={styles.title}>Nova transação</h6>
 
-      <form
-        className={styles.transactionForm}
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit();
-        }}
-      >
+      <form className={styles.transactionForm}>
         <select
           className="form-select mb-2"
           value={selectedOption}
-          onChange={(e) => onChangeOption(e.target.value)}
+          onChange={(e) => setSelectedOption(e.target.value)}
         >
           <option value="">Selecione o tipo de transação</option>
-          {options.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
+          {Object.entries(transactionOptions).map(([key, label]) => (
+            <option key={key} value={key}>
+              {label}
             </option>
           ))}
         </select>
@@ -59,14 +53,16 @@ export function TransactionForm({
           className="form-control mb-2"
           type="text"
           placeholder="00,00"
-          value={value}
-          onChange={(e) => onChangeValue(e.target.value)}
+          value={transactionValue}
+          onChange={(e) => setTransactionValue(e.target.value)}
         />
         <button
           className="btn btn-dark w-100"
           onClick={(e) => {
             e.preventDefault();
-            onSubmit();
+            onSubmit(selectedOption, transactionValue);
+            setSelectedOption("");
+            setTransactionValue("");
           }}
         >
           Concluir transação
