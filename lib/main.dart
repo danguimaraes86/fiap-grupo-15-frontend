1fc300326b7f0d717322a29a0a8a42c71d07463d
Daniel Guimarães <danguimaraes86@gmail.com>
2025-09-23 16:45:14 -0300

    add: firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bytebank/pages/home/cadastro_view.dart';
+import 'package:bytebank/configs/firebase_options.dart';
 import 'package:bytebank/pages/home/home_view.dart';
 import 'package:bytebank/pages/home/login_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
