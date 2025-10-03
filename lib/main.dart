71db9a71a4c27a0791e3c226abac1b20558c1f33
Daniel Guimarães <danguimaraes86@gmail.com>
2025-10-03 14:33:27 -0300

    refact: melhoria nos imports de config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:bytebank/configs/firebase_options.dart';
 import 'package:bytebank/configs/routes.dart';
 import 'package:bytebank/configs/system_colors.dart';
-import 'package:bytebank/pages/dashboard/dashboard_view.dart';
-import 'package:bytebank/pages/home/auth/cadastro_view.dart';
-import 'package:bytebank/pages/home/auth/login_view.dart';
-import 'package:bytebank/pages/home/home_view.dart';
-import 'package:bytebank/pages/transactions/transactions_form.dart';
-import 'package:bytebank/pages/transactions/transactions_list.dart';
 import 'package:bytebank/providers/transaction_provider.dart';
 import 'package:bytebank/providers/user_auth_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -36,27 +30,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ByteBank Fiap',
-      theme: ThemeData(
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: SystemColors.primary,
-          onPrimary: SystemColors.background,
-          secondary: SystemColors.secondary,
-          onSecondary: SystemColors.background,
-          error: Colors.yellowAccent,
-          onError: SystemColors.background,
-          surface: SystemColors.secondary,
-          onSurface: SystemColors.background,
-        ),
-      ),
-      routes: {
-        Routes.home: (context) => const HomeView(),
-        Routes.login: (context) => const LoginView(),
-        Routes.cadastro: (context) => const CadastroView(),
-        Routes.dashboard: (context) => const DashboardView(),
-        Routes.transactionForm: (context) => const TransactionsFormPage(),
-        Routes.transactionList: (context) => const TransactionsListPage(),
-      },
+      theme: ThemeData(colorScheme: SystemColors().colorScheme),
+      routes: Routes().routes,
     );
   }
 }
