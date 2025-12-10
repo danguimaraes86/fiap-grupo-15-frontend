a43f4dddc7b652e1e595d948657bdab837d32bfb
Daniel Guimarães <danguimaraes86@gmail.com>
2025-12-10 09:29:31 -0300

    add: loginModal e registerModal componentes

---
+++ src/app/components/login-modal/login-modal.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+import { MatError, MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { LoginRequest } from '../../models/request.model';
+import { AuthenticationService } from '../../services/authentication.service';
+import { FirebaseError } from 'firebase/app';
+
+@Component({
+  selector: 'app-login-modal',
+  imports: [
+    MatIcon,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatFormField,
+    MatLabel,
+    MatPrefix,
+    MatSuffix,
+    MatInput,
+    MatButton,
+    MatIconButton,
+    MatError,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login-modal.html',
+  styleUrl: './login-modal.css',
+})
+export class LoginModal {
+  loginForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder, private authService: AuthenticationService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Cadastro:', this.loginForm.value);
+      
+      this.authService.login(this.loginForm.value as LoginRequest)
+        .subscribe(user => console.log(user))
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+}
---
+++ src/app/components/register-modal/register-modal.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+import { MatError, MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { RegisterRequest } from '../../models/request.model';
+import { AuthenticationService } from '../../services/authentication.service';
+
+@Component({
+  selector: 'app-register-modal',
+  imports: [
+    MatIcon,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatFormField,
+    MatLabel,
+    MatPrefix,
+    MatSuffix,
+    MatInput,
+    MatButton,
+    MatIconButton,
+    MatCheckbox,
+    MatError,
+    ReactiveFormsModule
+  ],
+  templateUrl: './register-modal.html',
+  styleUrl: './register-modal.css',
+})
+export class RegisterModal {
+  registerForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder, private authService: AuthenticationService) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      acceptTerms: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Cadastro:', this.registerForm.value);
+
+      this.authService.register(this.registerForm.value as RegisterRequest)
+        .subscribe(user => console.log(user))
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+}
