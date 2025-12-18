99d4f48b3f0869d16bdb9d6273756c61fa2c4213
Daniel Guimarães <danguimaraes86@gmail.com>
2025-12-17 23:50:58 -0300

    add: initial dashboard and auth-guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 import { sessionUserGuard } from './guards/session-user.guard';
 
 export const routes: Routes = [
@@ -8,4 +9,9 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home-view/home-view').then(c => c.HomeView),
     canActivate: [sessionUserGuard]
   },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard-view/dashboard-view').then(c => c.DashboardView),
+    canActivate: [authGuard]
+  }
 ];
