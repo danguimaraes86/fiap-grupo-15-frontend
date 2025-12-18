d5798183463aacc5d3d02231cad55ad6e3fe8793
Daniel Guimarães <danguimaraes86@gmail.com>
2025-12-17 23:47:25 -0300

    add: authState observer and loading

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   Auth,
   AuthError,
+  browserLocalPersistence,
   createUserWithEmailAndPassword,
   User as FirebaseUser,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   updateProfile
 } from 'firebase/auth';
@@ -17,54 +20,84 @@ import { getFirebaseErrorMessage } from '../utils/firebase-errors.util';
 })
 export class AuthenticationService {
   private _firebaseAuth: Auth;
-  public currentUserSignal = signal<FirebaseUser | null>(null);
-  public authErrorMessage = signal<string | null>(null);
+  private _router = inject(Router)
+
+  private _userSignal = signal<FirebaseUser | null>(null);
+  public userSignal = this._userSignal.asReadonly()
+
+  private _authErrorMessage = signal<string | null>(null);
+  public authErrorMessage = this._authErrorMessage.asReadonly()
+
+  private _isLoading = signal<boolean>(true);
+  public isLoading = this._isLoading.asReadonly()
 
   constructor() {
     this._firebaseAuth = getAuth(firebaseApp)
+    this._firebaseAuth.setPersistence(browserLocalPersistence)
+    this.initAuthStateListener()
+  }
+
+  private initAuthStateListener() {
+    onAuthStateChanged(this._firebaseAuth, (user) => {
+      this._userSignal.set(user);
+      this._isLoading.set(false)
+    });
+  }
+
+  isAuthenticated(): boolean {
+    return this._userSignal() !== null;
   }
 
   async register(request: RegisterRequest): Promise<void> {
+    this._isLoading.set(true)
     try {
-      this.authErrorMessage.set(null)
+      this.clearAuthError()
       const credential = await createUserWithEmailAndPassword(
         this._firebaseAuth,
         request.email,
         request.password
       );
       await updateProfile(credential.user, { displayName: request.name });
-      this.currentUserSignal.set(credential.user)
     } catch (error) {
       this.handleAuthError(error as AuthError)
+    } finally {
+      this._isLoading.set(false)
     }
   }
 
   async login(request: LoginRequest): Promise<void> {
+    this._isLoading.set(true)
     try {
-      this.authErrorMessage.set(null);
-      const credential = await signInWithEmailAndPassword(
+      this.clearAuthError()
+      await signInWithEmailAndPassword(
         this._firebaseAuth,
         request.email,
         request.password
       );
-      this.currentUserSignal.set(credential.user)
     } catch (error) {
       this.handleAuthError(error as AuthError);
+    } finally {
+      this._isLoading.set(false)
     }
   }
 
   async logout(): Promise<void> {
     try {
       await this._firebaseAuth.signOut();
-      this.currentUserSignal.set(null);
-      this.authErrorMessage.set(null);
+      this._userSignal.set(null);
+      this._authErrorMessage.set(null);
+      this._router.navigate([''])
     } catch (error) {
       this.handleAuthError(error as AuthError);
     }
   }
 
+  clearAuthError() {
+    this._authErrorMessage.set(null);
+  }
+
   private handleAuthError(error: AuthError): void {
-    this.authErrorMessage.set(getFirebaseErrorMessage(error.code));
-    console.error('Firebase Auth Error:', error.code, this.authErrorMessage());
+    this._authErrorMessage.set(getFirebaseErrorMessage(error.code));
+    console.error('Firebase Auth Error:', error.code, this._authErrorMessage());
   }
 }
