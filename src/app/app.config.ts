91b13827c7a9de3de143349bc2b47cee73ef7fd2
Daniel Guimarães <danguimaraes86@gmail.com>
2026-01-05 12:13:08 -0300

    add: locale e currency pipe

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
 import { routes } from './app.routes';
 
+registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
--- src/app/components/summary-card/summary-card.ts
+++ src/app/components/summary-card/summary-card.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-summary-card',
   standalone: true,
-  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle],
+  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle, CurrencyPipe],
   templateUrl: './summary-card.html',
   styleUrl: './summary-card.css',
 })
@@ -12,11 +13,4 @@ export class SummaryCard {
   readonly totalReceitas = input(0);
   readonly totalDespesas = input(0);
   readonly saldo = input(0);
-
-  formatCurrency(value: number): string {
-    return new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
-    }).format(value);
-  }
 }
